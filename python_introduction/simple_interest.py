514e945d1617bab67760091781d0de0538981c8f
Jeremiah <jeremensz@gmail.com>
2025-09-14 16:25:44 +0000

    Add simple interest calculator script

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,13 @@
+# simple_interest.py
+# ALX Simple Interest Calculator
+
+# Variables
+principal = 1000
+rate = 0.05
+time = 3
+
+# Calculation using formula: I = P * R * T
+interest = principal * rate * time
+
+# Output
+print(f"The simple interest is: {interest}")
