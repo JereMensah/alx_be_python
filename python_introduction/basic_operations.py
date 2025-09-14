b8568ec29bc5aedbdc70c210929647be1fb2e621
Jeremiah <jeremensz@gmail.com>
2025-09-14 15:52:34 +0000

    Add basic_operations.py for ALX arithmetic task

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# basic_operations.py
+# ALX: Basic Arithmetic Exercise
+
+number1 = 12
+number2 = 6
+
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
